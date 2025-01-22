7e177dba5b40fe3a2479899868d3af8de1dd3cca
dinfuc510 <dauvaso18@gmail.com>
2025-01-22 18:24:13 +0700

    remove unused wait function

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,4 +1,3 @@
-// https://github.com/whileskies/xv6-labs-2020/blob/main/doc/Lab1-Xv6%20and%20Unix%20utilities.md
 #include "kernel/types.h"
 #include "user/user.h"
 
@@ -16,7 +15,6 @@ int main() {
 	}
 	else {
 		write(p[1], " ", 1);
-		wait(0);
 		read(p[0], &signal, 1);
 		printf("%d: received pong\n", getpid());
 	}
