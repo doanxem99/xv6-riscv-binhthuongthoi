ca96cfaae4ed1a6c30ea6fa6fc742a51dfad46a4
dinfuc510 <dauvaso18@gmail.com>
2025-01-24 23:38:40 +0700

    reduce numer of iterations

--- user/primes.c
+++ user/primes.c
@@ -5,14 +5,15 @@ void filter(int read_fd) {
 	short num = 0;
 	if (read(read_fd, &num, sizeof(num)) == 0) {
 		close(read_fd);
-		return;
+		exit(0);
 	}
+
+	short start = num;
+	printf("prime %d\n", start);
 
 	int child_fd[2];
 	pipe(child_fd);
 
-	short start = num;
-	printf("prime %d\n", start);
 	while (read(read_fd, &num, sizeof(num))) {
 		if (num % start != 0) {
 			write(child_fd[1], &num, sizeof(num));
@@ -28,21 +29,20 @@ void filter(int read_fd) {
 }
 
 int main() {
+	printf("prime 2\n");
+	printf("prime 3\n");
+
 	int parent_fd[2];
 	pipe(parent_fd);
 
-	for (short i = 2; i <= 257; i++) {
-		write(parent_fd[1], &i, sizeof(i));
+	for (short i = 5; i <= 280; i += 2) {
+		if (i % 3 != 0) {
+			write(parent_fd[1], &i, sizeof(i));
+		}
 	}
 	close(parent_fd[1]);
-
 	filter(parent_fd[0]);
 	wait(0);
 
-	// hard code :(
-	printf("prime 263\n");
-	printf("prime 269\n");
-	printf("prime 271\n");
-	printf("prime 277\n");
 	exit(0);
 }
