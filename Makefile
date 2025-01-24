24958229970d6db745612fc9317dc961c644a037
dinfuc510 <dauvaso18@gmail.com>
2025-01-24 22:14:27 +0700

    implement primes for 1-byte ints

--- Makefile
+++ Makefile
@@ -140,6 +140,7 @@ UPROGS=\
 	$U/_wc\
 	$U/_zombie\
 	$U/_pingpong\
+	$U/_primes\
 	$U/_find\
 	$U/_xargs\
 
---
+++ user/primes.c
@@ -0,0 +1,42 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+void filter(int read_fd) {
+	int child_fd[2];
+	pipe(child_fd);
+
+	char num = 0;
+	if (read(read_fd, &num, sizeof(num)) == 0) {
+		return;
+	}
+
+	char start = num;
+	printf("prime %d\n", start);
+	while (read(read_fd, &num, sizeof(num))) {
+		if (num % start != 0) {
+			write(child_fd[1], &num, sizeof(num));
+		}
+	}
+	close(read_fd);
+	close(child_fd[1]);
+
+	int is_child = (fork() == 0);
+	if (is_child) {
+		filter(child_fd[0]);
+	}
+}
+
+int main() {
+	int parent_fd[2];
+	pipe(parent_fd);
+
+	for (char i = 2; i <= 128; i++) {
+		write(parent_fd[1], &i, sizeof(i));
+	}
+	close(parent_fd[1]);
+
+	filter(parent_fd[0]);
+	wait(0);
+
+	exit(0);
+}
